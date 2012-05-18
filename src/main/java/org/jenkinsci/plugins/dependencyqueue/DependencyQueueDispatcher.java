c329a6e0422abc3594976bef392569a1fa721300
Larry Shatzer <larrys@gmail.com>
2012-05-18 12:02:43 -0600

    First commit

---
+++ src/main/java/org/jenkinsci/plugins/dependencyqueue/DependencyQueueDispatcher.java
@@ -0,0 +1,23 @@
+package org.jenkinsci.plugins.dependencyqueue;
+
+
+import hudson.Extension;
+import hudson.model.AbstractProject;
+import hudson.model.Hudson;
+import hudson.model.Queue;
+import hudson.model.queue.CauseOfBlockage;
+import hudson.model.queue.QueueTaskDispatcher;
+
+@Extension
+public class DependencyQueueDispatcher extends QueueTaskDispatcher {
+
+    @Override
+    public CauseOfBlockage canRun(Queue.Item itemInQuestion) {
+        for (Queue.Item queuedItem : Hudson.getInstance().getQueue().getItems()) {
+            if (((AbstractProject) queuedItem.task).getTransitiveDownstreamProjects().contains((AbstractProject) itemInQuestion.task)) {
+                return CauseOfBlockage.fromMessage(Messages._DependencyQueueDispatcher_UpstreamInQueue(queuedItem.task.getName()));
+            }
+        }
+        return super.canRun(itemInQuestion);
+    }
+}
