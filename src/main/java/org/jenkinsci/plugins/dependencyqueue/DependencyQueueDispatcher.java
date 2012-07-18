00d7f61a6f1f30f899d9aa77cc1683c67a34cfed
Stephen Connolly <stephen.alan.connolly@gmail.com>
2012-07-18 17:34:02 +0200

    Missed a second CCE

--- src/main/java/org/jenkinsci/plugins/dependencyqueue/DependencyQueueDispatcher.java
+++ src/main/java/org/jenkinsci/plugins/dependencyqueue/DependencyQueueDispatcher.java
@@ -13,9 +13,11 @@ public class DependencyQueueDispatcher extends QueueTaskDispatcher {
 
     @Override
     public CauseOfBlockage canRun(Queue.Item itemInQuestion) {
-        for (Queue.Item queuedItem : Hudson.getInstance().getQueue().getItems()) {
-            if (queuedItem.task instanceof AbstractProject && ((AbstractProject) queuedItem.task).getTransitiveDownstreamProjects().contains((AbstractProject) itemInQuestion.task)) {
-                return CauseOfBlockage.fromMessage(Messages._DependencyQueueDispatcher_UpstreamInQueue(queuedItem.task.getName()));
+        if (itemInQuestion instanceof AbstractProject) {
+            for (Queue.Item queuedItem : Hudson.getInstance().getQueue().getItems()) {
+                if (queuedItem.task instanceof AbstractProject && ((AbstractProject) queuedItem.task).getTransitiveDownstreamProjects().contains((AbstractProject) itemInQuestion.task)) {
+                    return CauseOfBlockage.fromMessage(Messages._DependencyQueueDispatcher_UpstreamInQueue(queuedItem.task.getName()));
+                }
             }
         }
         return super.canRun(itemInQuestion);
