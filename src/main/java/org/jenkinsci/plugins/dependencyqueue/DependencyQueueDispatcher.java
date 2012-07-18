a9a0a46020276a016030f4b12623ace24795c1a8
Stephen Connolly <stephen.alan.connolly@gmail.com>
2012-07-18 17:34:35 +0200

    I hate fixing things with github editor

--- src/main/java/org/jenkinsci/plugins/dependencyqueue/DependencyQueueDispatcher.java
+++ src/main/java/org/jenkinsci/plugins/dependencyqueue/DependencyQueueDispatcher.java
@@ -13,7 +13,7 @@ public class DependencyQueueDispatcher extends QueueTaskDispatcher {
 
     @Override
     public CauseOfBlockage canRun(Queue.Item itemInQuestion) {
-        if (itemInQuestion instanceof AbstractProject) {
+        if (itemInQuestion.task instanceof AbstractProject) {
             for (Queue.Item queuedItem : Hudson.getInstance().getQueue().getItems()) {
                 if (queuedItem.task instanceof AbstractProject && ((AbstractProject) queuedItem.task).getTransitiveDownstreamProjects().contains((AbstractProject) itemInQuestion.task)) {
                     return CauseOfBlockage.fromMessage(Messages._DependencyQueueDispatcher_UpstreamInQueue(queuedItem.task.getName()));
