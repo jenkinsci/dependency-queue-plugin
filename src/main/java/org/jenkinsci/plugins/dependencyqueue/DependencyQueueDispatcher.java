6f075bdd85f215369c14eb18aa3490cd17b9bdba
Larry Shatzer, Jr <larrys@gmail.com>
2015-12-23 12:20:06 -0700

    Update to a more recent LTS version, as well as remove deprecated usage.

--- src/main/java/org/jenkinsci/plugins/dependencyqueue/DependencyQueueDispatcher.java
+++ src/main/java/org/jenkinsci/plugins/dependencyqueue/DependencyQueueDispatcher.java
@@ -1,12 +1,11 @@
 package org.jenkinsci.plugins.dependencyqueue;
 
-
 import hudson.Extension;
 import hudson.model.AbstractProject;
-import hudson.model.Hudson;
 import hudson.model.Queue;
 import hudson.model.queue.QueueTaskDispatcher;
 import hudson.model.queue.CauseOfBlockage;
+import jenkins.model.Jenkins;
 
 @Extension
 public class DependencyQueueDispatcher extends QueueTaskDispatcher {
@@ -21,7 +20,7 @@ public class DependencyQueueDispatcher extends QueueTaskDispatcher {
             	//property is present and set to false.  Don't block.
                 return null;
             }
-            for (Queue.Item queuedItem : Hudson.getInstance().getQueue().getItems()) {
+            for (Queue.Item queuedItem : Jenkins.getInstance().getQueue().getItems()) {
                 if (queuedItem.task instanceof AbstractProject && ((AbstractProject) queuedItem.task).getTransitiveDownstreamProjects().contains(projectInQuestion)) {
                     return CauseOfBlockage.fromMessage(Messages._DependencyQueueDispatcher_UpstreamInQueue(queuedItem.task.getName()));
                 }
